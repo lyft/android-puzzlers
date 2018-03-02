c73ff3ac2c79d89f35e027d94e67b58be31289f6
Mark Wei <markwei@lyft.com>
2018-03-02 00:28:20 -0800

    Initial commit. (#1)
    PR=https://github.com/lyft/android-puzzlers/pull/1
    GIT_MULTI_BRANCH=_git-multi-pr--initial-commit-5F86E138-9925-487E-95F6-3615CAB00819

---
+++ 2018_03_08/intents/src/main/java/android/lyft/com/intents/MainActivity.java
@@ -0,0 +1,38 @@
+package android.lyft.com.intents;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    /* If this Activity is declared as android:launchMode=singleTask, what's printed when the user clicks the button? */
+
+    private static final String NAME = "NAME";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener((view) -> {
+            Intent intent = new Intent(view.getContext(), MainActivity.class);
+            intent.putExtra(NAME, "Joe");
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        trace(intent.getStringExtra(NAME));
+
+        trace(getIntent().getStringExtra(NAME));
+        super.onNewIntent(intent);
+        trace(getIntent().getStringExtra(NAME));
+    }
+
+    private void trace(String message) {
+        Log.d("Puzzlers", message != null ? message : "null");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# android_puzzlers_03_08_2018
-Android puzzles for the Lyft Talks: Android tech talk on 3/8/2018.
+# Android Puzzlers!
+
+## What is this
+
+This is a small set of Android puzzlers (code that is tricky, or may not work as you expect it to)
+
+## What to do
+
+- Clone the repo (to play with code!)
+- Open one of the sub projects in Android Studio
+- Find puzzlers in each module
+- Read comment in `MainActivity` describing what you need to figure out
+- Solve as many puzzlers as you can!
+- Share your results with link to this repo by mentioning [`@lyfteng`](https://twitter.com/lyfteng)
+- You can solve puzzlers with the whole team and use it as a little teambuilding event 😽
+- Good luck and have fun!
+
+## Explanations
+
+We're going to publish explanations of why and how puzzlers work in our [Engineering Blog](https://eng.lyft.com/), you can follow it on [Medium](https://eng.lyft.com/) or [Twitter](https://twitter.com/lyfteng)
+
+## License
+
+Apache 2.0 (see [LICENSE](LICENSE))
